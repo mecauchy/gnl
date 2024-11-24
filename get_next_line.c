04f862193ef630ac646a95d1b6956ad6f8ef532a
Melissa Cauchy--Samake <mcauchy-@paul-f5Br5s7.clusters.42paris.fr>
2024-11-24 16:51:41 +0100

    no_leaks_possible

--- get_next_line.c
+++ get_next_line.c
@@ -1,46 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcauchy- <mcauchy-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 11:19:24 by mcauchy-          #+#    #+#             */
+/*   Updated: 2024/11/24 16:38:36 by mcauchy-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*new_line(char *next_str)
 {
 	char	*str;
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 
 	i = 0;
-	j = 0;
 	if (!next_str)
 		return (NULL);
 	while (next_str[i] && next_str[i] != '\n')
 		i++;
-	str = (char *)malloc(sizeof(char) * ft_strlen(new_line) - i + 1);
-	if (!str)
+	if (!next_str[i])
 	{
 		free(next_str);
 		return (NULL);
 	}
+	str = (char *)malloc(sizeof(char) * ft_strlen(next_str) - i + 1);
+	if (!str)
+		return (free(next_str), NULL);
 	i++;
+	j = 0;
 	while (next_str[i])
-	{
-		str[j] = next_str[i];
-		i++;
-		j++;
-	}
+		str[j++] = next_str[i++];
 	str[j] = '\0';
+	free(next_str);
 	return (str);
 }
 
-char	*get_line(char *next_str)
+char	*ft_get_line(char *next_str)
 {
 	int		i;
 	char	*new_line;
 
 	i = 0;
-	if (!next_str)
+	if (!next_str || !next_str[i])
 		return (NULL);
 	while (next_str[i] != '\n' && next_str[i])
 		i++;
-	new_line = malloc(sizeof(char) * i + 2);
-	if (new_line == 2)
+	new_line = (char *)malloc(sizeof(char) * i + 2);
+	if (!new_line)
 		return (NULL);
 	i = 0;
 	while (next_str[i] != '\n' && next_str[i])
@@ -48,11 +59,12 @@ char	*get_line(char *next_str)
 		new_line[i] = next_str[i];
 		i++;
 	}
-	if (next_str[i] == '\n')
+	if (next_str[i] && next_str[i] == '\n')
 	{
-		new_line[i] = '\n';
-		new_line[i + 1] = '\0';
+		new_line[i] = next_str[i];
+		i++;
 	}
+	new_line[i] = '\0';
 	return (new_line);
 }
 
@@ -60,24 +72,26 @@ char	*read_to_next_str(char *next_str, int fd)
 {
 	int		rd_bytes;
 	char	*buff;
-	char	*new_str;
 
-	rd_bytes = -1;
+	rd_bytes = 1;
 	buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
 		return (NULL);
-	while (!(ft_strchr(next_str, '\n')) && rd_bytes != 0)
+	while ((!ft_find_newline(next_str)) && rd_bytes > 0)
 	{
 		rd_bytes = read(fd, buff, BUFFER_SIZE);
-		if (rd_bytes <= 0)
+		if ((rd_bytes == -1) || (rd_bytes == 0 && next_str == NULL))
 		{
+			free(next_str);
+			next_str = NULL;
 			free(buff);
 			return (NULL);
 		}
-		next_str = '\0';
-		new_str = ft_strjoin(next_str, buff);
+		buff[rd_bytes] = '\0';
+		next_str = ft_strjoin(next_str, buff);
 	}
-	return (new_str);
+	free(buff);
+	return (next_str);
 }
 
 char	*get_next_line(int fd)
@@ -85,12 +99,19 @@ char	*get_next_line(int fd)
 	static char	*next_str = NULL;
 	char		*ret;
 
-	if (fd < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	next_str = ft_read_to_next_str(next_str, fd);
+	next_str = read_to_next_str(next_str, fd);
 	if (!next_str)
 		return (NULL);
-	ret = get_line(next_str);
-	next_str = new_line(next_str);
+	ret = ft_get_line(next_str);
+	if (!ret && next_str && *next_str != '\0')
+	{
+		ret = ft_strdup(next_str);
+		free(next_str);
+		next_str = NULL;
+	}
+	else
+		next_str = new_line(next_str);
 	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,33 +6,36 @@
 /*   By: mcauchy- <mcauchy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 16:55:13 by mcauchy-          #+#    #+#             */
-/*   Updated: 2024/11/21 17:49:47 by mcauchy-         ###   ########.fr       */
+/*   Updated: 2024/11/24 16:41:54 by mcauchy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 
-#define GET_NEXT_LINE_H	
+# define GET_NEXT_LINE_H	
 
-
-#ifndef BUFFER_SIZE
-
-#define BUFFER_SIZE 42
-
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE	1
+# endif
 
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
 
+// GET_LINE //
+
 char	*get_next_line(int fd);
-int		ft_strchr(const char *s, int c);
+char	*read_to_next_str(char *next_str, int fd);
+char	*ft_get_line(char *next_str);
+char	*new_line(char *next_str);
 
 // UTILS //
 
-int	ft_strlen(char *str)
-char	ft_substr()
-
+int		ft_strlen(char *str);
+//int		ft_find_newline(char *s);
+char	*ft_find_newline(char *s);
+char	*ft_strjoin(char *next_str, char *buff);
+char	*ft_strdup(char *s1);
 
 #endif
